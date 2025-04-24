bfcb8674b3cd8f135e7c0d7e56238846d85ec8b4
Dr.Lt.Data <dr.lt.data@gmail.com>
2025-04-24 18:00:37 +0900

    feat: Select Nth Item (Any list)
    https://github.com/ltdrdata/ComfyUI-Impact-Pack/issues/971

--- js/impact-pack.js
+++ js/impact-pack.js
@@ -331,6 +331,32 @@ app.registerExtension({
 			}
 		}
 
+		if(nodeData.name == "ImpactSelectNthItemOfAnyList") {
+			const onConnectionsChange = nodeType.prototype.onConnectionsChange;
+			nodeType.prototype.onConnectionsChange = function (type, index, connected, link_info) {
+				if(!link_info || this.inputs[0].type != '*')
+					return;
+
+				if(index >= 2)
+					return;
+
+				// assign type
+				let slot_type = '*';
+
+				if(type == 2) {
+					slot_type = link_info.type;
+				}
+				else {
+					const node = app.graph.getNodeById(link_info.origin_id);
+					slot_type = node.outputs[link_info.origin_slot].type;
+				}
+
+				this.inputs[0].type = slot_type;
+				this.outputs[0].type = slot_type;
+				this.outputs[0].label = slot_type;
+			}
+		}
+
 		if(nodeData.name === 'ImpactInversedSwitch') {
 			nodeData.output = ['*'];
 			nodeData.output_is_list = [false];
